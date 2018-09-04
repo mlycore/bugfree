d720ea4baab536de16e930b8259c8d8ce3677689
mlycore <maxwell92@126.com>
2018-09-05 00:02:01 +0800

    updated program

--- datastructures/list/bucketsort.go
+++ datastructures/list/bucketsort.go
@@ -6,7 +6,10 @@ import (
 
 func pow(a, b int) int {
 	p := 1
-	for i := 0; i < b; i++ {
+	if b == 0 {
+		return 1
+	}
+	for i := 1; i < b+1; i++ {
 		p = p * a
 	}
 	return p
@@ -18,33 +21,30 @@ func BucketSort(list []int) []int {
 	for i := 0; i < 10; i++ {
 		buckets[i] = make([]int, 0)
 	}
+	loc := make(map[int]int)
 
-	for i:=1; i<10; i++ {
-		for j:=0; j<len(list); j++ {
+	for i := 0; i < 10; i++ {
+		for j := 0; j < len(list); j++ {
 			if list[j] == 0 {
 				continue
 			}
 			t := list[j] / pow(10, i)
-			rem := list[j] % 10
-			buckets[rem] = append(buckets[rem], list[j])
-			if t == 0 {
-				list[j] = 0		
-			} else {
-				buckets[rem] = buckets[rem][0:len(buckets[rem]) - 1]
+			rem := t % 10
+			if i > 0 && loc[list[j]] != rem {
+				buckets[loc[list[j]]] = buckets[loc[list[j]]][0 : len(buckets[loc[list[j]]])-1]
+
 			}
-			fmt.Printf("t: %d, rem: %d, buckets: %v\n", t, rem, buckets[rem])
-			fmt.Printf("buckets: %v\n", buckets)
-		}
+			if loc[list[j]] == rem {
+				continue
+			}
+			loc[list[j]] = rem
+			buckets[rem] = append(buckets[rem], list[j])
 
-	}
-
-	result := make([]int, len(list))
-	var k int
-	for i := 0; i < len(buckets); i++ {
-		for j := 0; j < len(buckets[i]); j++ {
-			result[k] = buckets[i][j]
-			k++
+			if t == 0 {
+				list[j] = 0
+			}
+			fmt.Printf("i: %d, t: %d, rem: %d, buckets: %v\n", i, t, rem, buckets)
 		}
 	}
-	return result
+	return buckets[0]
 }
