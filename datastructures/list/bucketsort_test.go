bec70143f53dc28970976c87fced073ea64dbfb2
mlycore <maxwell92@126.com>
2018-09-05 23:10:26 +0800

    add unpassed case

--- datastructures/list/bucketsort_test.go
+++ datastructures/list/bucketsort_test.go
@@ -10,3 +10,10 @@ func Test_BucketSort(t *testing.T) {
 	r := BucketSort(list)
 	fmt.Printf("%v\n", r)
 }
+
+func Test_BucketSort_1(t *testing.T) {
+	list := []int{212, 4, 36, 19, 8, 11}
+	fmt.Printf("%v\n", list)
+	r := BucketSort(list)
+	fmt.Printf("%v\n", r)
+}
