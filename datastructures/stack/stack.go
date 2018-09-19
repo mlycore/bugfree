bd2b9ef3f98a7a2ef628e0f4200f650839e7e94d
mlycore <maxwell92@126.com>
2018-09-19 20:44:42 +0800

    add stack

--- datastructures/stack/stack.go
+++ datastructures/stack/stack.go
@@ -1,1 +1,28 @@
 package stack
+
+type Elem struct {
+	Data string
+}
+
+type Stack struct{
+	Elems []Elem
+	Top int
+}
+
+func (s *Stack)Push(e Elem) {
+
+}
+
+func (s *Stack)Pop() Elem {
+
+}
+
+func (s *Stack)IsEmpty() bool {
+
+}
+
+func (s *Stack)IsFull() bool {
+
+}
+
+
