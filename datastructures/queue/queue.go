46fc93a9c410738e68027ad3e2a4ff833700f4e1
mlycore <maxwell92@126.com>
2018-09-20 23:02:37 +0800

    updated datastructure

--- datastructures/queue/queue.go
+++ datastructures/queue/queue.go
@@ -1,1 +1,27 @@
 package queue
+
+type Elem struct {
+	Data string
+}
+
+type Queue struct {
+	Front int
+	Rear int
+	Elems []Elem
+}
+
+func(q *Queue) IsEmpty() bool {
+
+}
+
+func(q *Queue) IsFull() bool {
+
+}
+
+func(q *Queue) Push(e Elem) {
+
+}
+
+func(q *Queue) Pop() Elem {
+
+}
